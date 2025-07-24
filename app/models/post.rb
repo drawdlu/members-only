ad7f15e9b3c0d19bdd8e4224c6d79c182bc44a2a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-07-25 05:10:40 +0800

    feat: Accommodate more characters in body of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 10, maximum: 40 }
-  validates :body, presence: true, length: { minimum: 30, maximum: 150 }
+  validates :body, presence: true, length: { minimum: 30, maximum: 900 }
 end
